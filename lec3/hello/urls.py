d42de4e99401b2e100966ce7bd7c3a60cc89146a
OrangePapermoon <hughtyou@gmail.com>
2026-04-22 13:58:41 +0900

    Add unfinished web app from lecture 3

--- lec3/hello/urls.py
+++ lec3/hello/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
+    # any str inside <> would be passed to the greet function as a parameter
+    path("hugh", views.hugh, name="hugh"),
+    path("david", views.david, name="david")
+
 ]
--- lec3/hello/views.py
+++ lec3/hello/views.py
@@ -3,4 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
+    # rendering html files instead of hardcode it here allows us to seperate python files and html files
+
+def hugh(request):
+    return HttpResponse("Hello, Hugh!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize() # Here we pass all the information to that specific html file using a dictionary
+    })
+
