5fa67f2998bdd7ae09d76041afba7c57f0e9fc12
OrangePapermoon <hughtyou@gmail.com>
2026-05-12 22:29:43 +0900

    Add mainly API and JSON related content from Lecture 5

--- lec5/counter.js
+++ lec5/counter.js
@@ -1,18 +1,21 @@
-let counter = 0;
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+}
 
 function count() {
+    let counter = localStorage.getItem('counter');
     counter++;
     document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter);
 
-    if (counter % 10 === 0) {
-        alert(`Count is now ${counter}`);
-    }
 }
 document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
     // it's not count() because we're not calling the count function now
     // we're just setting the onclick property of the querySelector('button')
     // to count, which is just the name of the function
     // the count function is only called when the button is actually clicked
     // this is a paradigm of functional programming 
+
 });
